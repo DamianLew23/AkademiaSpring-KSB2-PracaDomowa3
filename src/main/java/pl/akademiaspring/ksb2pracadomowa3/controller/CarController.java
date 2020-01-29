db398e562391846d0c20845ffe12db84b4642218
Damian Lew <dlew8912@gmail.com>
2020-01-29 15:33:30 +0100

    Final Commit

--- src/main/java/pl/akademiaspring/ksb2pracadomowa3/controller/CarController.java
+++ src/main/java/pl/akademiaspring/ksb2pracadomowa3/controller/CarController.java
@@ -1,11 +1,71 @@
 package pl.akademiaspring.ksb2pracadomowa3.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.akademiaspring.ksb2pracadomowa3.model.Car;
+import pl.akademiaspring.ksb2pracadomowa3.service.CarService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/cars")
 public class CarController {
 
+    private CarService carService;
+
+    @Autowired
+    CarController(
+            CarService carService
+    ) {
+        this.carService = carService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Car>> getCars(
+            @RequestParam(required = false) String color) {
+        if (color == null) {
+            return new ResponseEntity<>(carService.getCars(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(carService.getCarsByColor(color), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Car> getCarById(@PathVariable long id) {
+        Car car = carService.getCarById(id);
+        return new ResponseEntity<>(car, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Car> addCar(@RequestBody Car car) {
+        if (carService.addCar(car)){
+            return new ResponseEntity<>(car, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @PutMapping
+    public ResponseEntity<Car> modCar(@RequestBody Car car) {
+        if (carService.modCar(car)){
+            return new ResponseEntity<>(car, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Car> removeCar(@PathVariable long id) {
+        if (carService.removeCar(id)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+     }
+
+     @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity handleNoSuchElementExceptio(NoSuchElementException e) {
+        return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+     }
 
 }
--- src/main/java/pl/akademiaspring/ksb2pracadomowa3/service/CarService.java
+++ src/main/java/pl/akademiaspring/ksb2pracadomowa3/service/CarService.java
@@ -5,6 +5,8 @@ import pl.akademiaspring.ksb2pracadomowa3.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class CarService {
@@ -15,6 +17,50 @@ public class CarService {
         carList.add(new Car(1L,"Volvo", "S60", "Black"));
         carList.add(new Car(2L,"Peugeot", "406", "White"));
         carList.add(new Car(3L,"Audi", "Q7", "Blue"));
-        carList.forEach(System.out::println);
     }
+
+
+    public List<Car> getCars() {
+        return this.carList;
+    }
+
+    public Car getCarById(long id) {
+        return carList
+                .stream()
+                .filter(x -> (x.getId() == id))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Nie znaleziono samochodu o ID: " + id));
+    }
+
+    public List<Car> getCarsByColor(String color) {
+        return carList
+                .stream()
+                .filter(car -> car.getColor().equals(color))
+                .collect(Collectors.toList());
+    }
+
+    public boolean addCar(Car car) {
+        return carList.add(car);
+    }
+
+    public boolean modCar(Car car) {
+        Car carFromList = carList
+                .stream()
+                .filter(x -> (x.getId() == car.getId()))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Nie znaleziono samochodu o ID: " + car.getId()));
+        carList.remove(carFromList);
+        return carList.add(car);
+    }
+
+    public boolean removeCar(long id) {
+        Car carFromList = carList
+                .stream()
+                .filter(car -> (car.getId() == id))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Nie znaleziono samochodu o ID: " + id));
+        return carList.remove(carFromList);
+    }
+
+
 }
